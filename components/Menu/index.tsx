2716d5e62d700785284069540ce5f169df5335ef
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2024-03-08 11:50:48 +0100

    add lastminute.com logo

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 const Menu: React.FC = () => {
     return (
         <nav className={'menu'}>
+            <div className={'logoContainer'}>
+                <img src="https://res.cloudinary.com/lastminute-contenthub/v1/DAM/Logos%20%2B%20fonts/SVG/lm_without%20border_NEW" alt="lastminute.com" className={'logo'}/>
+            </div>
             <ul className={'menuList'}>
                 <li className={'menuItem'}><Link href="/">Home</Link></li>
                 <li className={'menuItem'}><Link href="/partite">Partite</Link></li>
