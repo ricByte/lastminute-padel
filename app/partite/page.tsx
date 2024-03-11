190b219dd1fa3086197447336e09c2954a5f693a
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2024-03-11 13:44:33 +0100

    Use api to print

--- app/partite/page.tsx
+++ app/partite/page.tsx
@@ -4,8 +4,7 @@ import React, {useEffect, useState} from 'react';
 import {api} from "@/convex/_generated/api";
 import "@/app/globals.css";
 import Link from "next/link";
-import {Button} from "@/components/ui/button";
-import {useAction, useMutation} from "convex/react";
+import {useQuery} from "convex/react";
 
 const PartitePage: React.FC = () => {
     const [nowPlaying, setNowPlaying] = useState(false);
@@ -13,70 +12,48 @@ const PartitePage: React.FC = () => {
     useEffect(() => {
         // Controlla l'orario corrente e imposta lo stato di nowPlaying
         const interval = setInterval(() => {
-            const now = new Date();
+            const now = new Date().toISOString();
 
-            const isBetween = now >= partite[0].startDate && now <= partite[0].endDate;
+            const isBetween = !!gamesForToday && now >= gamesForToday[0].startDate && now <= gamesForToday[0].endDate;
             setNowPlaying(isBetween);
         }, 1000); // Controlla ogni secondo
 
         return () => clearInterval(interval); // Pulisce l'intervallo quando il componente viene smontato
     }, []);
 
-    const partite = [
-        {
-            squadra1: 'Team A',
-            squadra2: 'Team B',
-            punteggioSquadra1: 2,
-            punteggioSquadra2: 1,
-            vincitore: 'Team A', // Aggiungi il vincitore della partita
-            startDate: new Date(2024, 2, 8, 8, 0),
-            endDate: new Date(2024, 2, 12, 17, 0),
-        },
-        // Aggiungi altre partite...
-    ];
+    // const performMyAction = useMutation(api.myFunctions.addGames);
+    // const addGame = () => {
+    //     performMyAction({
+    //         endDate: "2024-03-10T17:30:00.000Z",
+    //         startDate: "2024-03-12T18:00:00.000Z",
+    //         team1: "Team1",
+    //         team2: "Team2"
+    //     }).then((r)=>console.log(r))
+    //         .catch(()=>console.log("KO"))
+    // };
+    // const performMyAction2 = useAction(api.myFunctions.retrieveGames);
+    // const retrieveGames = () => {
+    //     performMyAction2({date: new Date().toISOString()}).then((r)=>console.log(r))
+    //         .catch(()=>console.log("KO"))
+    // };
 
-    // const performMyAction = useAction(api.myFunctions.doSomething);
-    const performMyAction = useMutation(api.myFunctions.addGames);
-    const performMyAction2 = useAction(api.myFunctions.retrieveGames);
-    const retrieveGames = () => {
-        performMyAction2({date: new Date().toISOString()}).then((r)=>console.log(r))
-            .catch(()=>console.log("KO"))
-    };
-    const addGame = () => {
-        performMyAction({
-            endDate: "2024-03-10T17:30:00.000Z",
-            startDate: "2024-03-12T18:00:00.000Z",
-            team1: "Team1",
-            team2: "Team2"
-        }).then((r)=>console.log(r))
-            .catch(()=>console.log("KO"))
-    };
+    const gamesForToday = useQuery(api.myFunctions.gamesForDay, {date:new Date().toISOString()})
+
     return (
         <div className={'partite-container'}>
-            <h1>Partite</h1>
-            <Button
-                onClick={addGame}
-            >
-                Add Game
-            </Button>
-            <Button
-                onClick={retrieveGames}
-            >
-                retrieveGames
-            </Button>
-
+            <h1>Partite di oggi</h1>
             <div className={'partita-grid'}>
-                {partite.map((partita, index) => (
+                {gamesForToday && gamesForToday.map((partita, index) => (
                     <div key={index} className={'partita-item'}>
                         <p className={'partita-details'}>
-                            {partita.squadra1} vs {partita.squadra2} @{partita.startDate.toLocaleString()}
+                            {partita.team1} vs {partita.team2} @{partita.startDate.toLocaleString()}
                         </p>
                         <p className={'partita-details'}>
-                            {partita.punteggioSquadra1} - {partita.punteggioSquadra2}
+                            {partita.pointsTeam1} - {partita.pointsTeam2}
                             {nowPlaying && <span
                                 className={'now-playing'}>Now Playing</span>} {/* Didascalia "Now Playing" se nell'orario indicato */}
                         </p>
-                        <p>Vincitore: {partita.vincitore}</p> {/* Mostra il vincitore */}
+                        <p>Vincitore: {partita.winner}</p> {/* Mostra il vincitore */}
                     </div>
                 ))}
             </div>
