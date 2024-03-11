e982d27306d702b7b246ee6186efda4b4db4d416
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2024-03-11 23:55:44 +0100

    Test

---
+++ app/partite/team/[team]/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
+import {api} from "@/convex/_generated/api";
+import "@/app/globals.css";
+import Link from "next/link";
+import {useAction} from "convex/react";
+import {PersistedGame} from "@/convex/myFunctions";
+import {NextRouter, useRouter} from 'next/router';
+
+const PartitePage: React.FC = () => {
+    const actionRetrieve = useAction(api.myFunctions.retrieveGames);
+    const router: NextRouter = useRouter()
+
+    const [nowPlaying, setNowPlaying] = useState(false);
+    const [gamesForToday, setGamesForToday]: [PersistedGame[]|undefined, Dispatch<SetStateAction<PersistedGame[]|undefined>>] = useState();
+
+    useEffect(()=> {
+        const callback = () => {
+            actionRetrieve({date: 124567})
+                .then(newVar => {
+                    if(newVar) setGamesForToday(newVar)
+                })
+                .catch(reason => console.log(reason))
+        };
+        const interval = setInterval( callback, 60*1000); // Controlla ogni minuto
+        callback();
+        return () => clearInterval(interval); // Pulisce l'intervallo quando il componente viene smontato
+    }, []);
+
+    useEffect(() => {
+        // Controlla l'orario corrente e imposta lo stato di nowPlaying
+        const interval = setInterval(() => {
+            const now = new Date().toISOString();
+
+            const isBetween = !!gamesForToday && now >= gamesForToday[0].startDate && now <= gamesForToday[0].endDate;
+            setNowPlaying(isBetween);
+        }, 1000); // Controlla ogni secondo
+
+        return () => clearInterval(interval); // Pulisce l'intervallo quando il componente viene smontato
+    }, []);
+
+    useEffect(() => {
+
+    }, [gamesForToday]);
+
+    // const performMyAction = useMutation(api.myFunctions.addGames);
+    // const addGame = () => {
+    //     performMyAction({
+    //         endDate: "2024-03-10T17:30:00.000Z",
+    //         startDate: "2024-03-12T18:00:00.000Z",
+    //         team1: "Team1",
+    //         team2: "Team2"
+    //     }).then((r)=>console.log(r))
+    //         .catch(()=>console.log("KO"))
+    // };
+    // const performMyAction2 = useAction(api.myFunctions.retrieveGames);
+    // const retrieveGames = () => {
+    //     performMyAction2({date: new Date().toISOString()}).then((r)=>console.log(r))
+    //         .catch(()=>console.log("KO"))
+    // };
+
+    return (
+        <div className={'partite-container'}>
+            <div className={'padel-intro'}>
+                <h1 className={'padel-title'}>Partite di oggi</h1>
+                <p>team: {router.query.team}</p>
+                <p>query: {JSON.stringify(router.query)}</p>
+            </div>
+            <div className={'partita-grid'}>
+                {gamesForToday?.map((partita, index) => {
+                    const startDate = new Date(partita.startDate);
+                    return (
+                        <div key={index} className={'partita-item'}>
+                            <p className={'partita-details'}>
+                                {partita.team1} vs {partita.team2} @{startDate.toLocaleString()}
+                            </p>
+                            <p className={'partita-details'}>
+                                {partita.pointsTeam1} - {partita.pointsTeam2}
+                                {nowPlaying && <span
+                                    className={'now-playing'}>Now Playing</span>} {/* Didascalia "Now Playing" se nell'orario indicato */}
+                            </p>
+                            <p>Vincitore: {partita.winner}</p> {/* Mostra il vincitore */}
+                        </div>
+                    );
+                })}
+            </div>
+            <Link href="/">Torna alla home</Link>
+        </div>
+    );
+};
+
+export default PartitePage;
