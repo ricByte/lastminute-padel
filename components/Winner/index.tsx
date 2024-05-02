7f51d325508da705ad6146122bec5c0eda76bfd6
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2024-05-02 12:38:33 +0200

    another try

--- components/Winner/index.tsx
+++ components/Winner/index.tsx
@@ -1,25 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import '../../app/globals.css';
-import {FaTrophy} from "react-icons/fa6";
-import { ReactConfetti as Confetti } from "react-confetti";
+import FaTrophy from "@/components/FaTrophy";
 
 interface WinnerProps {
 
 }
 
 export const Winner: React.FC<WinnerProps> = () => {
-    const [showConfetti, setShowConfetti] = useState(false);
-
-    useEffect(() => {
-        setShowConfetti(true);
-        setTimeout(() => {
-            setShowConfetti(false);
-        }, 3000);
-    }, []);
 
     return (
         <div className={'vincitore-container'}>
-            {showConfetti && <Confetti />}
             <FaTrophy className={'trophy-icon'} />
             <div className={'vincitore-info'}>
                 <h2>Team 20</h2>
